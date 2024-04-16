556d6a8fd1ca616d603fbc01983bf33398f69d78
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-04-16 11:02:55 -0300

    g

---
+++ App-Quiz-Javascript-master/index.js
@@ -0,0 +1,279 @@
+const $startGameButton = document.querySelector(".start-quiz")
+const $nextQuestionButton = document.querySelector(".next-question")
+const $questionsContainer = document.querySelector(".questions-container")
+const $questionText = document.querySelector(".question")
+const $answersContainer = document.querySelector(".answers-container")
+const $answers = document.querySelectorAll(".answer")
+
+let pontuacaoGrupoUm = 0
+let pontuacaoGrupoDois=0
+
+
+
+
+let currentQuestionIndex = 0
+let totalCorrect = 0
+
+
+
+const questions = [
+  {
+    question: "Dentro de qual elemento HTML colocamos o JavaScript?",
+    answers: [
+      { text: "<javascript>", correct: false },
+      { text: "<js>", correct: false },
+      { text: "<script>", correct: true },
+      { text: "<scripting>", correct: false }
+    ]
+  },
+  {
+    question: "Onde é o lugar correto para inserir JavaScript?",
+    answers: [
+      { text: "Tanto no <head> quanto no <body> está correto", correct: true },
+      { text: "No <body>", correct: false },
+      { text: "No <head>", correct: false },
+      { text: "Em outro lugar", correct: false }
+    ]
+  },
+  {
+    question: 'Qual é a sintaxe correta para se referir a um script externo chamado "xxx.js"',
+    answers: [
+      { text: '<script src="xxx.js">', correct: true },
+      { text: '<script href="xxx.js">', correct: false },
+      { text: '<script name="xxx.js">', correct: false },
+      { text: "Nenhuma das alternativas", correct: false }
+    ]
+  },
+  {
+    question: 'O arquivo JavaScript externo deve conter a tag <script>',
+    answers: [
+      { text: "Verdadeiro", correct: false },
+      { text: "Falso", correct: true }
+    ]
+  },
+  {
+    question: 'Como escrever "Hello World" numa caixa de alerta?',
+    answers: [
+      { text: 'msg("Hello World");', correct: false },
+      { text: 'alert("Hello World");', correct: true },
+      { text: 'msgBox("Hello World");', correct: false },
+      { text: 'alertBox("Hello World");', correct: false }
+    ]
+  },
+  {
+    question: 'Como podemos criar uma função no JavaScript?',
+    answers: [
+      { text: 'function:myFunction()', correct: false },
+      { text: 'function myFunction()', correct: true },
+      { text: 'function = myFunction()', correct: false },
+      { text: 'Nenhum desses códigos criaria uma função', correct: false }
+    ]
+  },
+  {
+    question: 'Como podemos chamar uma função chamada "minhaFuncao"?',
+    answers: [
+      { text: 'call minhaFuncao()', correct: false },
+      { text: 'call function minhaFuncao()', correct: false },
+      { text: 'Nenhum desses códigos chamaria essa função', correct: false },
+      { text: 'minhaFuncao()', correct: true },
+    ]
+  },
+]
+
+
+
+
+const btnEviarNome = document.querySelector("#EnviarNome")
+const nomeUm = document.querySelector("#nome1")
+const nomeDois = document.querySelector("#nome2")
+const form = document.querySelector("#form")
+const conteiner = document.querySelector(".container") 
+const quemComeca = document.querySelector("#quemComeca")
+const equipeComeca = document.querySelector("#equipeComeca")
+const divForm = document.querySelector(".letra")
+const equipeResponde = document.querySelector("#equipeResponde")
+const divRespode = document.querySelector(".nomesResponde")
+const header = document.querySelector("header")
+header.classList.add("hide")
+btnEviarNome.addEventListener("click",(e)=>{
+  e.preventDefault()
+  const nomeEquipeUm = nomeUm.value
+  const nomeEquipeDois = nomeDois.value
+  divRespode.classList.add("hide")
+  nomesDeTimes(nomeEquipeUm,nomeEquipeDois)
+  
+  hide()
+  play()
+
+  $startGameButton.addEventListener("click", startGame)
+  $nextQuestionButton.addEventListener("click", displayNextQuestion)
+
+function startGame() {
+  $startGameButton.classList.add("hide")
+  $questionsContainer.classList.remove("hide")
+  $answersContainer.classList.add("hide")
+  displayNextQuestion()
+}
+
+function displayNextQuestion() {
+  resetState()
+  
+  if (questions.length === currentQuestionIndex) {
+    return finishGame()
+  }
+
+  $questionText.textContent = questions[currentQuestionIndex].question
+  questions[currentQuestionIndex].answers.forEach(answer => {
+    const newAsnwer = document.createElement("button")
+    newAsnwer.classList.add("button", "answer")
+    newAsnwer.textContent = answer.text
+    if (answer.correct) {
+      
+      newAsnwer.dataset.correct = answer.correct
+    }
+    $answersContainer.appendChild(newAsnwer)
+
+    newAsnwer.addEventListener("click", selectAnswer)
+  })
+}
+
+function resetState() {
+  while($answersContainer.firstChild) {
+    $answersContainer.removeChild($answersContainer.firstChild)
+  }
+
+  document.body.removeAttribute("class")
+  $nextQuestionButton.classList.add("hide")
+}
+
+function selectAnswer(event) {
+  const answerClicked = event.target
+
+  if (answerClicked.dataset.correct) {
+    if(nomeEquipeUm == nomeEquipeUm){
+      pontuacaoGrupoUm +=1
+    }else if(nomeEquipeDois == nomeEquipeDois){
+      pontuacaoGrupoDois+=1
+    }
+    document.body.classList.add("correct")
+    totalCorrect++
+  } else {
+    if(nomeEquipeUm == nomeEquipeUm){
+      if(pontuacaoGrupoUm >0){
+        pontuacaoGrupoUm -=1
+      }
+    }else if(nomeEquipeDois == nomeEquipeDois){
+      if(pontuacaoGrupoDois >0){
+        pontuacaoGrupoDois -=1
+      }
+    }
+    document.body.classList.add("incorrect") 
+  }
+
+  document.querySelectorAll(".answer").forEach(button => {
+    button.disabled = true
+
+    if (button.dataset.correct) {
+      button.classList.add("correct")
+    } else {
+      button.classList.add("incorrect")
+    }
+  })
+  
+  $nextQuestionButton.classList.remove("hide")
+  currentQuestionIndex++
+}
+
+function finishGame() {
+  const totalQuestions = questions.length
+  const performance = Math.floor(totalCorrect * 100 / totalQuestions)
+  
+  let message = ""
+
+  switch (true) {
+    case (performance >= 90):
+      message = "Excelente :)"
+      break
+    case (performance >= 70):
+      message = "Muito bom :)"
+      break
+    case (performance >= 50):
+      message = "Bom"
+      break
+    default:
+      message = "Pode melhorar :("
+  }
+
+  $questionsContainer.innerHTML = 
+  `
+    <p class="final-message">
+      Você acertou ${totalCorrect} de ${totalQuestions} questões!
+      <span>Resultado: ${message}</span>
+    </p>
+    <button 
+      onclick=window.location.reload() 
+      class="button"
+    >
+      Refazer teste
+    </button>
+  `
+}
+
+
+
+
+
+  function play(){
+    document.body.addEventListener("keyup",(e)=>{
+      
+      const EquipeStart = equipeComeca.value
+      primeiraLetra(EquipeStart)
+      e.preventDefault()
+    })
+  }
+  function primeiraLetra(equipe){
+    const letra = equipe[0]
+    divForm.classList.add("hide")
+    $answersContainer.classList.remove("hide")
+    divRespode.classList.remove("hide")
+    header.classList.remove("hide")
+    verificar(letra)
+  
+  
+  }
+  function verificar (letra){
+    if(letra == "a"){
+      equipeResponde.textContent = `${nomeEquipeUm} Responde`
+
+    }else if (letra == "l"){
+      equipeResponde.textContent = `${nomeEquipeDois} Responde`
+    }
+  }
+  
+})
+
+function hide(){
+  form.style.display = "none"
+  conteiner.style.display = "flex"
+}
+
+function nomesDeTimes (nomeUm,NomeDois){
+  if(nomeUm == nomeUm){
+    pontuacaoGrupoUm +=1
+  }else if (NomeDois == NomeDois){
+    pontuacaoGrupoDois +=1
+  }
+  
+}
+function pontuacao (){
+  
+}
+
+
+
+
+
+
+
+
+
