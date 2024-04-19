3e535949d90319e5db51f8039afa810c43ca38f8
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-04-19 15:23:29 -0300

    full code

--- App-Quiz-Javascript-master/index.js
+++ App-Quiz-Javascript-master/index.js
@@ -8,12 +8,8 @@ const $answers = document.querySelectorAll(".answer")
 let pontuacaoGrupoUm = 0
 let pontuacaoGrupoDois=0
 
-function randomNumber (){
-  return Math.floor(Math.random()*6)
-  
-}
-let NumRandom = randomNumber()
-console.log(NumRandom)
+
+
 
 
 let currentQuestionIndex = 0
@@ -21,19 +17,7 @@ let indexQuestionDois = 0
 
 
 
-const desafios = [{desafio:" Coloque músicas animadas e faça uma versão improvisada da dança das cadeiras, onde os alunos devem dançar ao redor das cadeiras enquanto a música toca e sentar quando ela parar"},
-{ desafio: "Divida a turma em equipes e faça um jogo de mímica, onde cada equipe deve representar uma palavra ou frase engraçada sem usar palavras."
-},{
-  desafio:"Peça aos alunos para desenharem algo na lousa com os olhos vendados. Você pode dar instruções simples, como desenhar um animal, uma casa ou um rosto. O resultado geralmente é hilário e pode render boas risadas"
-},{
-  desafio:"Comece uma história engraçada e peça aos alunos para continuá-la, um por vez, adicionando um trecho cada um"
-},{
-  desafio:"Peça aos alunos para formarem duplas e criarem trajes improvisados usando papel, fita adesiva e outros materiais disponíveis na sala de aula."
-},{
-  desafio:"Divida a turma em grupos e peça a cada grupo para criar um trocadilho engraçado usando uma palavra específica que você fornecer. Depois, cada grupo deve apresentar seu trocadilho para a classe e ver quem consegue arrancar mais risadas."
-}]
-let challenge = desafios[NumRandom].desafio
-console.log(challenge)
+
 
 const questions = [
   {
@@ -46,57 +30,59 @@ const questions = [
     ]
   },
   {
-    question: "Como as questões de saúde mudam ou se tornam mais relevantes durante a meia-idade?",
+    question: "Qual das seguintes mudanças pode ocorrer na vida social durante a meia-idade?",
     answers: [
-      { text: "Aumento do risco de doenças cardíacas", correct: true },
-      { text: "Diminuição do risco de osteoporose", correct: false },
-      { text: "Redução do risco de diabetes tipo 2", correct: false },
-      { text: "Melhora da função pulmonar", correct: false }
+      { text: "Maior envolvimento em atividades comunitárias", correct: true },
+      { text: "Diminuição da necessidade de interação social", correct: false },
+      { text: "Redução do contato com amigos e familiares", correct: false },
+      { text: "Aumento da participação em festas e eventos sociais", correct: false }
     ]
   },
   {
-    question: 'Qual é a sintaxe correta para se referir a um script externo chamado "xxx.js"',
+    question: 'Qual das seguintes atividades pode ser benéfica para a saúde mental durante a meia-idade?',
     answers: [
-      { text: '<script src="xxx.js">', correct: true },
-      { text: '<script href="xxx.js">', correct: false },
-      { text: '<script name="xxx.js">', correct: false },
-      { text: "Nenhuma das alternativas", correct: false }
+      { text: 'Participação em grupos de apoio', correct: true },
+      { text: 'Isolamento social', correct: false },
+      { text: 'Prática regular de exercícios físicos intensos', correct: false },
+      { text: "Ignorar sentimentos de tristeza e ansiedade", correct: false }
     ]
   },
   {
-    question: 'O arquivo JavaScript externo deve conter a tag <script>',
+    question: 'Durante a meia-idade, qual das seguintes atividades pode ser benéfica para a saúde física?',
     answers: [
-      { text: "Verdadeiro", correct: false },
-      { text: "Falso", correct: true }
+      { text: "Ficar sedentário por longos períodos", correct: false },
+      { text: "Praticar exercícios de alta intensidade sem descanso", correct: false },
+      { text: "Ignorar sinais de dor e desconforto", correct: false },
+      { text: "Praticar atividades fisicas regularmente", correct: true }
     ]
   },
   {
-    question: 'Como escrever "Hello World" numa caixa de alerta?',
+    question: 'Durante a meia-idade, qual das seguintes opções é uma preocupação comum relacionada à saúde física??',
     answers: [
-      { text: 'msg("Hello World");', correct: false },
-      { text: 'alert("Hello World");', correct: true },
-      { text: 'msgBox("Hello World");', correct: false },
-      { text: 'alertBox("Hello World");', correct: false }
+      { text: 'Aumento da massa muscula', correct: false },
+      { text: 'Diminuição da flexibilidade', correct: true },
+      { text: 'Melhoria da memória', correct: false },
+      { text: 'Aumento da taxa metabólica', correct: false }
     ]
   }
 ]
 
 const questionDois = [ {
-  question: "Qual das seguintes preocupações é comum para muitas pessoas durante a meia-idade em relação aos pais idosos?",
+  question: "Durante a meia-idade, qual desses fatores geralmente não contribui para mudanças emocionais?",
   answers: [
-    { text: "Melhora na visão de perto", correct: false },
-    { text: "Redução da capacidade auditiva", correct: false },
-    { text: "Perda de cabelo", correct: true },
-    { text: "Aumento da densidade óssea", correct: false }
+    { text: "Transição de carreira", correct: false },
+    { text: "Início de novos relacionamentos amorosos", correct: false },
+    { text: " Aumento da estabilidade financeira", correct: true },
+    { text: "Vazio do ninho (filhos saindo de casa)", correct: false }
   ]
 },
 {
-  question: "Durante a meia-idade, qual das seguintes atividades pode contribuir para a saúde física e mental?",
+  question: "Qual das seguintes condições de saúde é mais comum durante a meia-idade?",
   answers: [
-    { text: "Melhora na visão de perto", correct: false },
-    { text: "Redução da capacidade auditiva", correct: false },
-    { text: "Perda de cabelo", correct: true },
-    { text: "Aumento da densidade óssea", correct: false }
+    { text: "Sarampo", correct: false },
+    { text: "Acne", correct: false },
+    { text: "Diabetes tipo 2", correct: true },
+    { text: "Catapora", correct: false }
   ]
 },
 {
@@ -109,21 +95,21 @@ const questionDois = [ {
   ]
 },
 {
-  question: "Durante a meia-idade, qual das seguintes atividades pode ser benéfica para a saúde física?",
+  question: "Durante a meia-idade, quais desses fatores podem contribuir para um aumento do estresse?",
   answers: [
-    { text: "Melhora na visão de perto", correct: false },
-    { text: "Redução da capacidade auditiva", correct: false },
-    { text: "Perda de cabelo", correct: true },
-    { text: "Aumento da densidade óssea", correct: false }
+    { text: "Redução das responsabilidades familiares", correct: false },
+    { text: "Estabilidade financeira", correct: false },
+    { text: "Preocupações com a saúde dos pais idosos", correct: true },
+    { text: "Melhoria na qualidade do sono", correct: false }
   ]
 },
 {
-  question: "Durante a meia-idade, qual é uma preocupação comum relacionada à vida profissional?",
+  question: "Qual é uma das principais preocupações financeiras durante a meia-idade?",
   answers: [
-    { text: "Melhora na visão de perto", correct: false },
-    { text: "Redução da capacidade auditiva", correct: false },
-    { text: "Perda de cabelo", correct: true },
-    { text: "Aumento da densidade óssea", correct: false }
+    { text: "Pagar pela faculdade dos filhos", correct: false },
+    { text: "Comprar uma casa pela primeira vez", correct: false },
+    { text: "Planejar para a aposentadoria", correct: true },
+    { text: "Economizar para uma viagem de férias de luxo", correct: false }
   ]
 },
 ]
@@ -159,6 +145,7 @@ btnEviarNome.addEventListener("click",(e)=>{
   let nomeEquipeDois = nomeDois.value
   divRespode.classList.add("hide")
   let selectedValue = selected.value
+  
   
 
 
@@ -230,22 +217,32 @@ function selectAnswer(event) {
   const answerClicked = event.target
 
   if (answerClicked.dataset.correct) {
+
     pontuacao(nomeEquipeUm)
     pontuacaoGrupoDoiss(nomeEquipeDois)
-
     document.body.classList.add("correct")
     document.querySelectorAll(".answer").forEach(button => {
+      if(button.dataset.correct){
+        button.classList.add("correct")
+      }else{
+        button.classList.add("incorrect")
+      }
+      
       button.disabled = true
     })
-    
-    
-    
   } else {
+      
+    
       perdePontoGrupoUm(nomeEquipeUm)
       perdePontoGrupoDois(nomeEquipeDois)
 
     document.body.classList.add("incorrect") 
     document.querySelectorAll(".answer").forEach(button => {
+      if(!button.dataset.correct){
+        button.classList.add("incorrect")
+      }else{
+        button.classList.add("correct")
+      }
       button.disabled = true
     })
     if(equipeResponde.textContent == nomeEquipeUm){
@@ -278,16 +275,10 @@ function finishGame() {
     $questionsContainer.innerHTML = 
     `
       <p class="final-message">
-        Grupo ${nomeEquipeDois} Ganhou!
+        Grupo <span>${nomeEquipeDois}</span> Ganhou!
         
       </p>
-      <p class="final-message">
-        A equipe ${nomeEquipeUm} Tera que Pagar um DESAFIO!
-      </p>
-      <h1>DESAFIO</h1>
-      <p class="final-message">
-        "${challenge}
-      </p>
+      
 
       <button 
         onclick=window.location.reload()
@@ -300,16 +291,11 @@ function finishGame() {
     $questionsContainer.innerHTML = 
     `
       <p class="final-message">
-        Grupo ${nomeEquipeUm} Ganhou!
+        Grupo <span>${nomeEquipeUm}</span> Ganhou!
         
       </p>
-      <p class="final-message">
-        A equipe ${nomeEquipeDois} Tera que Pagar um DESAFIO!
-      </p>
-      <h1>DESAFIO</h1>
-      <p class="final-message">
-        "${challenge}
-      </p>
+      
+      
       <button 
         onclick=window.location.reload()
         class="button"
@@ -321,7 +307,7 @@ function finishGame() {
     $questionsContainer.innerHTML =  ` 
     <h1>EMPATE</h1>
     <p class="final-message">
-      Nimguem paga!
+      Nimguem Ganhou!
     </p>
     
     <button 
@@ -344,13 +330,13 @@ function finishGame() {
 
   function play(){
     document.body.addEventListener("keyup",(e)=>{
-      
       const EquipeStart = equipeComeca.value
       primeiraLetra(EquipeStart)
       e.preventDefault()
     })
   }
   function primeiraLetra(equipe){
+    
     const letra = equipe[0]
     divForm.classList.add("hide")
     $answersContainer.classList.remove("hide")
